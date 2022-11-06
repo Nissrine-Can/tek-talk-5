f51ea7a9c889c255d60e1f8a97677d8bb42512eb
Nissrine Canina <niss.canina@gmail.com>
2022-11-06 11:33:24 -0500

    updated header modal visibility state

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
+import AuthModal from './AuthModal';
+import AuthModalContext from './AuthModalContext';
+import UserContext from './UserContext';
+import BoardHeader from './BoardHeader';
+import Header from './Header';
+import PostForm from './PostForm';
+import { useState, useEffect } from 'react';
+import Post from './Post';
 
 
 function App() {
+
+  const [showAuthModal, setShowAuthModal] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    if(token) {
+      fetch('/get-current-user', {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': `bearer ${token}`
+        }
+      })
+      .then(resp => resp.json())
+      .then(data => setCurrentUser(data))
+    };
+  }, [])
+
+  function logout(){
+    setCurrentUser({})
+  }
+
   return (
-    <div className="App">
-      test
-    </div>
+    <AuthModalContext.Provider value={{show: showAuthModal, setShow: setShowAuthModal}}>
+      <UserContext.Provider value={{...currentUser, logout, setCurrentUser}}>
+          <Header />
+          <AuthModal />
+          <BoardHeader />
+          <PostForm />
+          <Post />
+      </UserContext.Provider>
+    </AuthModalContext.Provider>
   );
 }
 
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import ClickOutHandler from 'react-clickout-handler';
 import { HiOutlineCode,
          HiSearch,
          HiOutlineBell,
          HiOutlineChat,
          HiPlusSm,
-         HiChevronDown
+         HiChevronDown,
+         HiOutlineUser,
+         HiLogin,
+         HiLogout
         } from "react-icons/hi";
+import AuthModalContext from './AuthModalContext';
 import avatar from './avatar.png';
+import Button from './Button';
+import UserContext from './UserContext';
 
 const Header = () => {
-  return (
-    <div>
-        <header className='w-full bg-app_dark p-2'>
 
-            <div className='mx-4 flex'>
+  const [userDropdownVisibilityClass, setUserDropdownVisibilityClass] = useState('hidden')
+  
+  function toggleUserDropdown() {
+    if (userDropdownVisibilityClass === 'hidden') {
+       setUserDropdownVisibilityClass('block')
+    } else {
+       setUserDropdownVisibilityClass('hidden')
+    }
+  };
+
+  const authModal = useContext(AuthModalContext);
+  const user = useContext(UserContext);
+  
+  return (
+    
+      <header className='w-full bg-app_dark p-2'>
+
+            <div className='mx-4 flex relative'>
 
                 <HiOutlineCode className='w-8 h-8 mr-4 text-white' />
 
@@ -29,27 +50,88 @@ const Header = () => {
 
                 </form>
 
-                <button className='px-2 py-1'>
-                    <HiOutlineBell className='text-gray-400 w-6 h-6 mx-2'/>
-                </button>
-                <button className='px-2 py-1'>
-                    <HiOutlineChat className='text-gray-400 w-6 h-6 mx-2'/>
-                </button>
-                <button className='px-2 py-1'>
-                    <HiPlusSm className='text-gray-400 w-6 h-6 mx-2'/>
-                </button>
+                {user.username && (
+                  <>
+                    <button className='px-2 py-1'>
+                      <HiOutlineChat className='text-gray-400 w-6 h-6 mx-2'/>
+                    </button>
+                    <button className='px-2 py-1'>
+                      <HiOutlineBell className='text-gray-400 w-6 h-6 mx-2'/>
+                    </button>
+                    <button className='px-2 py-1'>
+                      <HiPlusSm className='text-gray-400 w-6 h-6 mx-2'/>
+                    </button>
+                  </>
+                )}
 
-                <button className='rounded-md flex ml-4'>
-                  <div className='w-8 h-8 bg-gray-600 rounded-md'> 
-                    <img src={avatar} alt='' style={{filter:'invert(100%)'}} className='block'/>
-                  </div>
-                  <HiChevronDown className='text-gray-500 w-5 h-5 mt-2 ml-1'/>
-                </button>
+                {!user.username && (
+                   <div className='mx-2 hidden sm:block'> 
+                      <Button 
+                        outline='true' 
+                        className='mr-1 h-8'
+                        onClick={() => authModal.setShow('login')}
+                      >Log In</Button>
+                      <Button 
+                        className='h-8'
+                        onClick={() => authModal.setShow('register')}
+                      >Sign Up</Button>
+                 </div>
+                )}
+                
+                
+
+                <ClickOutHandler 
+                    onClickOut={()=> setUserDropdownVisibilityClass('hidden')}>
+                  <button 
+                      className='rounded-md flex ml-4 border border-gray-700'
+                      onClick={() => {toggleUserDropdown()}}
+                  >
+                    {!user.username && (
+                      <HiOutlineUser className='w-6 h-6 m-1 text-gray-400'/>
+                    )}
+                    {user.username && (
+                      <div className='w-8 h-8 bg-gray-600 rounded-md'> 
+                         <img src={avatar} alt='' style={{filter:'invert(100%)'}} className='block'/> 
+                      </div> 
+                    )}
+                    
+                    <HiChevronDown className='text-gray-500 w-5 h-5 mt-2 m-1'/>
+                  </button>
+
+                  <div className={'absolute right-0 top-8 bg-app_dark border border-gray-700 z-10 rounded-md text-app_text overflow-hidden '+userDropdownVisibilityClass}> 
+                      {user.username && (
+                        <span className='w-50 py-2 px-3 text-sm'>
+                          Hello, {user.username}!
+                        </span>
+                      )}
+
+                      {!user.username && (
+                        <button
+                        className='flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm'
+                        onClick={() => authModal.setShow('login')}>
+                          <HiLogin className='w-5 h-5 mr-2'/>
+                          Log In / Sign Up
+                        </button>
+                      )}
+                      {user.username && (
+                        <button
+                        className='flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm'
+                        onClick={() => user.logout()}>
+                          <HiLogout className='w-5 h-5 mr-2'/>
+                          Logout
+                        </button>
+                      )}
+                          
+                    </div>
+
+                </ClickOutHandler>
+
+                
 
             </div>
 
-        </header>
-    </div>
+      </header>
+  
   )
 }
 
