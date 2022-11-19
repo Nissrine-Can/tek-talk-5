23fbe09caf8ff8460809898c2b89fbd70e4a6a31
Nissrine Canina <niss.canina@gmail.com>
2022-11-19 14:36:44 -0500

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
     has_many :posts, dependent: :destroy
 
+    has_many :comments
+    has_many :posts, through: :comments
+
     
     validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: { case_sensitive: false }
