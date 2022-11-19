c91ac1fca7db626e89761680d28a45ecdb44e480
Nissrine Canina <niss.canina@gmail.com>
2022-11-19 14:35:21 -0500

    added comments actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+    before_action :set_post
+
+
+    def index 
+        render json: Comment.all.order(created_at: :desc)
+    end
+
+    def create 
+        @comment = @post.comments.build(comment_params)
+        if @comment.save
+            render json: @comment, status: :created
+        else
+            render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def set_post
+        @post = Post.find_by_id(params[:post_id])
+    end
+
+    def comment_params
+        params.permit(:body, :user_id)
+    end
+
+
+end
