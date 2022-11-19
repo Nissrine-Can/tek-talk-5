41e45ecc3f96e4601bd6668944c9f97eef453209
Nissrine Canina <niss.canina@gmail.com>
2022-11-19 14:51:43 -0500

    seeded user and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+require 'faker';
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'user'
+User.create(username: 'test123', email: 'test123@email.com', password: '123')
+puts 'posts..'
+
+5.times do
+    Post.create(
+        user_id: 1,
+        title: Faker::Lorem.sentence,
+        body: Faker::Lorem.paragraph(sentence_count: 20),
+        posted_at: DateTime.new(2022,11,rand(1..6),rand(1..19),rand(1.60))
+    )
+end
