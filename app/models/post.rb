1a24fd028357204277e7b40d5ce18c66203d03ad
Nissrine Canina <niss.canina@gmail.com>
2022-11-19 14:36:25 -0500

    added associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  has_many :comments
+  has_many :users, through: :comments
+
+  accepts_nested_attributes_for :comments, reject_if: :all_blank
+  
+  
+
+  #validates: :body, presence: true
 end
