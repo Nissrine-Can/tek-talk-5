97bfebeccab4dae8465cf6ad32baa4e8294a1289
Nissrine Canina <niss.canina@gmail.com>
2022-11-19 14:37:50 -0500

    added custom route to display post creation time

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,14 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :posted_at
   has_one :user
+
+  has_many :comments, serializer: CommentSerializer
+
+
+  def posted_at
+    #binding.pry
+   self.object.created_at
+  end
+
+
 end
