befa77f653a736586875622c1e3060f1d8d1108a
Nissrine Canina <niss.canina@gmail.com>
2022-12-08 10:09:34 -0500

    added search params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,30 @@ class PostsController < ApplicationController
     skip_before_action :authorized, only: [:index, :show]
 
     def index
-        posts = Post.all.order(created_at: :desc)
-        render json: posts
+        
+        @posts = Post.all.order(created_at: :desc)
+           
+        if params[:search]
+        @posts = posts.where('lower(body) LIKE ?', "%#{params[:search].downcase}%")
+        elsif params[:name]
+        @topic_name = Topic.find_by(name: params[:name])
+        @posts = @topic_name.posts
+        end
+        render json: @posts, status: :ok
     end
 
     def show
         if @post
-            render json: @post
+            render json: @post, include: [comments: { include: [:user]}]
         else
             render json: {message: "Item not found!"}, status: :bad_request
         end
     end
 
     def create
-        @post = Post.new(post_params)
+        @post = Post.create(post_params)
+        @post.topic_posts.create({post_id: @post.id, topic_id: params['topic_ids'][0]})
+        
         # post.user_id = current_user.id
         if @post.save
             render json: @post, status: :ok
@@ -25,6 +35,31 @@ class PostsController < ApplicationController
         end
     end
 
+    def my_posts
+        render json: current_user.posts, status: :ok
+    end
+
+    def update
+        if @post
+          if @video.update(post_params)
+            render json: @post, status: :ok
+          else
+            render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { errors: "Your post is not updating" }, status: :bad_request
+        end
+      end
+    
+      def destroy
+        if @post
+          @post.destroy
+          render json: @post, status: :ok
+        else
+          render json: { errors: "Post delete propblem" }, status: :bad_request
+        end
+      end
+    
 
 
     private
@@ -34,7 +69,7 @@ class PostsController < ApplicationController
     end
 
     def post_params
-        params.require(:post).permit(:title, :body, :user_id)
+        params.require(:post).permit(:title, :body, :user_id, topic_ids: [])
     end
 
 
