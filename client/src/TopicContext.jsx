3657f39ca61f4d03174de5b8f43388270a07517a
Nissrine Canina <niss.canina@gmail.com>
2022-12-08 18:06:19 -0500

    added topic context

---
+++ client/src/TopicContext.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+export const TopicContext = React.createContext({})
+
+export function TopicContextProvider({children}) {
+    const [show, setShow] = useState(false);
+    const [topic, setTopic] = useState(null);
+    const [topicInfo, setTopicInfo] = useState({});
+    //const [topics, setTopics] = useState([])
+    const { name } = useParams();
+    useEffect(() => {
+       if(!name){
+        return;
+       }
+       const token = localStorage.getItem('jwt');
+            if(token) {
+            fetch(`/topics/${name}`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `bearer ${token}`
+                  }
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                console.log('communityDetails', data)
+                setTopicInfo(data)
+            })
+    }
+    }, [name, topic])
+
+    return (
+        <TopicContext.Provider value={{show, setShow, topic, setTopic, ...topicInfo}} >
+           {children}
+        </TopicContext.Provider>
+    )
+}
