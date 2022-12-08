f6d44c6ee2952ac391f0d251a3ec378f5c8a93db
Nissrine Canina <niss.canina@gmail.com>
2022-12-08 10:13:54 -0500

    added custom method upvotes and downvotes

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,14 +1,27 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :posted_at
+  attributes :id, :title, :body, :posted_at, :upvotes, :downvotes, :score
   has_one :user
-
   has_many :comments, serializer: CommentSerializer
 
+  has_many :topics
+  
+
+  def upvotes
+    self.object.votes.where(upvote: true)
+  end
+  def downvotes
+    self.object.votes.where(upvote: false)
+  end
+
+  def score
+    upvotes.size - downvotes.size
+  end
 
   def posted_at
-    #binding.pry
    self.object.created_at
   end
 
+  
+  
 
 end
