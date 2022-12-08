3b05c3a46a53efc0102a518a933d10d56fe70034
Nissrine Canina <niss.canina@gmail.com>
2022-12-08 10:17:03 -0500

    Board update

--- client/src/Board.jsx
+++ client/src/Board.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import BoardHeader from './BoardHeader'
 import PostForm from './PostForm'
 import PostsList from './PostsList'
+//import { useParams } from 'react-router-dom';
+//import { TopicContext } from './TopicContext';
 
 const Board = () => {
+  //const { name } = useParams();
+  
+  
+  //console.log("topicnameFromUrl", name);
+  //const {topic, setTopic} = useContext(TopicContext);
+
+  // useEffect(() =>{
+  //    setTopic(name);
+     
+  // }, [name, topic, setTopic])
+
   return (
     <div>
         <BoardHeader />
