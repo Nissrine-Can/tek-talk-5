9c6f45d1bd90d78532b594bc342b1a692eca6425
Nissrine Canina <niss.canina@gmail.com>
2022-12-08 10:12:02 -0500

    updated post associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,30 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
+  has_many :topic_posts
+  has_many :topics, through: :topic_posts
 
   has_many :comments
   has_many :users, through: :comments
 
+  has_many :votes
+  has_many :users, through: :votes
+
   accepts_nested_attributes_for :comments, reject_if: :all_blank
-  
+  #validates: :body, presence: true
+
   
 
-  #validates: :body, presence: true
+  # def score #diff between upvote and downvote
+  #   binding.pry
+  #   if self.upvoted_posts.length > 0 || self.downvoted_posts.length > 0
+  #     self.upvoted_posts.length > 0 ? (self.upvoted_posts.length - self.downvoted_posts.length) : (self.downvoted_posts.length * -1)
+  #   else
+  #     0
+  #   end
+
+
+  #end
+
+
 end
