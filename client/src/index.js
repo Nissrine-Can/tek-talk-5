40982406beacdc4b1e94f5abd11b114a9723d035
Nissrine Canina <niss.canina@gmail.com>
2022-12-08 17:55:19 -0500

    added MUI style theme

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { ThemeProvider } from "@material-tailwind/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Router>
 );
 
