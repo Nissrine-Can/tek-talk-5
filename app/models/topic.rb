835e50041c6bcc3f22681632403efbad3285e09d
Nissrine Canina <niss.canina@gmail.com>
2022-12-14 08:44:32 -0500

    added validations

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,6 +8,6 @@ class Topic < ApplicationRecord
     has_many :topic_users
     has_many :users, through: :topic_users
     
- 
+    validates :name, presence: true,uniqueness: { case_sensitive: false }
 
 end
